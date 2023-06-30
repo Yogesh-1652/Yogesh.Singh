48c4da0c8dd58cab4afdb0099f962765a9af7a71
Yogesh Singh <singhyug1509@gmail.com>
2023-07-01 03:51:57 +0530

    Initial commit

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer","Programmer", "Designer", "Freelancer"],
+        strings: ["Web Developer","Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer","Programmer", "Designer", "Freelancer"],
+        strings: ["Web Developer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
